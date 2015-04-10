5c5f0fb47547f8be373cefaf1922e79b8b1c9ebe
guiduck <guiduck02@gmail.com>
2015-04-10 19:53:33 -0300

    finished identing action.cpp

--- src/whiteboard/action.cpp
+++ src/whiteboard/action.cpp
@@ -1,20 +1,18 @@
 /*
- Copyright (C) 2010 - 2015 by Gabriel Morin <gabrielmorin (at) gmail (dot) com>
- Part of the Battle for Wesnoth Project http://www.wesnoth.org
+* Copyright (C) 2010 - 2015 by Gabriel Morin <gabrielmorin (at) gmail (dot) com>
+* Part of the Battle for Wesnoth Project http://www.wesnoth.org
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY.
+*
+* See the COPYING file for more details.
+*/
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY.
-
- See the COPYING file for more details.
- */
-
-/**
- * @file
- */
+// @file
 
 #include "action.hpp"
 #include "move.hpp"
@@ -22,7 +20,6 @@
 #include "recruit.hpp"
 #include "recall.hpp"
 #include "suppose_dead.hpp"
-
 #include "config.hpp"
 #include "game_board.hpp"
 #include "resources.hpp"
@@ -56,22 +53,29 @@ config action::to_config() const
 	return final_cfg;
 }
 
-/* static */
+// static 
 action_ptr action::from_config(config const& cfg, bool hidden)
 {
 	std::string type = cfg["type"];
 
-	try {
+	try 
+	{
 		if(type=="move")
+		{
 			return action_ptr(new move(cfg,hidden));
-		else if(type=="attack")
-			return action_ptr(new attack(cfg,hidden));
-		else if(type=="recruit")
-			return action_ptr(new recruit(cfg,hidden));
-		else if(type=="recall")
-			return action_ptr(new recall(cfg,hidden));
-		else if(type=="suppose_dead")
-			return action_ptr(new suppose_dead(cfg,hidden));
+		}	else if(type=="attack") 
+				 {
+					return action_ptr(new attack(cfg,hidden));
+				 }	else if(type=="recruit") 
+				 		 { 
+							return action_ptr(new recruit(cfg,hidden));
+						 } else if(type=="recall") 
+						 		{
+									return action_ptr(new recall(cfg,hidden));
+								}	else if(type=="suppose_dead")
+										 {
+											return action_ptr(new suppose_dead(cfg,hidden));
+										 }
 	} catch(action::ctor_err const&) {}
 
 	return action_ptr();
@@ -80,7 +84,9 @@ action_ptr action::from_config(config const& cfg, bool hidden)
 void action::hide()
 {
 	if(hidden_)
+	{
 		return;
+	}
 	hidden_ = true;
 	do_hide();
 }
@@ -88,7 +94,9 @@ void action::hide()
 void action::show()
 {
 	if(!hidden_)
+	{
 		return;
+	}
 	hidden_ = false;
 	do_show();
 }
@@ -104,10 +112,12 @@ action::action(config const& cfg, bool hidden)
 	, hidden_(hidden)
 {
 	// Construct and validate team_index_
-	int team_index_temp = cfg["team_index_"].to_int(-1); //default value: -1
+	int team_index_temp = cfg["team_index_"].to_int(-1); // default value: -1
 	if(team_index_temp < 0
 			|| team_index_temp >= static_cast<int>(resources::teams->size()))
+	{
 		throw ctor_err("action: Invalid team_index_");
+	}
 	team_index_ = team_index_temp;
 }
 
